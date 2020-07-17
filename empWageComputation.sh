95752589afa925e208cbc7d2736333e1d7bc90e3
“manishwadkar” <wadkarmanish@gmail.com>
2020-07-17 14:14:43 +0530

    UC5-Calculate-Wages-for-month implemented

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,21 +10,22 @@ WagePerHour=20
 FullDayHour=8
 wage=0
 ParttimeHour=4
+WorkingDaysPerMonth=20
 
 case "$attendance" in
 1)
-	wage=$(($WagePerHour*$FullDayHour))
+	wage=$(($WagePerHour*$FullDayHour*$WorkingDaysPerMonth))
 	echo "Employee is present"
-	echo "Daily wage is " $wage
+	echo "Monthly wage is " $wage
 ;;
 2)
-	wage=$(($WagePerHour*$ParttimeHour))
+	wage=$(($WagePerHour*$ParttimeHour*$WorkingDaysPerMonth))
         echo "Employee is present (PART TIME)"
-        echo "Daily wage is " $wage
+        echo "Monthly wage is " $wage
 ;;
 0)
 	echo "Employee is absent"
-	echo "Daily wage is " $wage
+	echo "Monthly wage is " $wage
 ;;
 *)
 	echo "Invalid option"
