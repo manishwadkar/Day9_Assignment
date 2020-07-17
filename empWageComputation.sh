901660a1029cc2533895dc4d501fec13d4ed6334
“manishwadkar” <wadkarmanish@gmail.com>
2020-07-17 13:35:24 +0530

    UC1-Check-Employee-Attendance implemented

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to employee wage computation program"
+
+isPresent=1
+
+attendance=$((RANDOM%2))
+
+if [ $attendance -eq $isPresent ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
