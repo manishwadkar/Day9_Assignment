89c26f262c859e538ece2222417bb5ef0b75fc83
“manishwadkar” <wadkarmanish@gmail.com>
2020-07-17 14:00:28 +0530

    UC3-Add-Part-time-Employee-Wage implemented

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,17 +4,23 @@ echo "Welcome to employee wage computation program"
 
 isPresent=1
 
-attendance=$((RANDOM%2))
+attendance=$((RANDOM%3))
 
 WagePerHour=20
 FullDayHour=8
 wage=0
+ParttimeHour=4
 
 if [ $attendance -eq $isPresent ]
 then
 	wage=$(($WagePerHour*$FullDayHour))
 	echo "Employee is present"
 	echo "Daily wage is " $wage
+elif [ $attendance -eq 2 ]
+then
+	wage=$(($WagePerHour*$ParttimeHour))
+        echo "Employee is present (PART TIME)"
+        echo "Daily wage is " $wage
 else
 	echo "Employee is absent"
 	echo "Daily wage is " $wage
